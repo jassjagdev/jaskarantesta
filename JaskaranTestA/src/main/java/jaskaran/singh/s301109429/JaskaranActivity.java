38f164466b7cddb79fda82270452eea039f032eb
Jass <jsingh321@gatech.edu>
2020-10-31 10:59:22 -0400

    first commit

---
+++ JaskaranTestA/src/main/java/jaskaran/singh/s301109429/JaskaranActivity.java
@@ -0,0 +1,22 @@
+package jaskaran.singh.s301109429;
+
+/*Jaskaran Singh 301109429 Sec 002*/
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class JaskaranActivity extends AppCompatActivity {
+
+}
---
+++ JaskaranTestA/src/main/java/jaskaran/singh/s301109429/SinghActivity.java
@@ -0,0 +1,7 @@
+package jaskaran.singh.s301109429;
+
+/*Jaskaran Singh 301109429 Sec 002*/
+
+public class SinghActivity {
+
+}
